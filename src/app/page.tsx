7a44051a62ba03f603e71651beb4c4ac66b97ebf
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-02-15 18:41:29 -0500

    style: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import AdminPanel from "@/components/AdminPanel"
 
 export default function Home() {
   return (
-      <div className="min-h-screen bg-background">
-        <header className="border-b border-border">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-foreground">Minecraft Server Admin Panel</h1>
-          </div>
-        </header>
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <AdminPanel />
-          </div>
-        </main>
-      </div>
+    <div className="min-h-screen bg-neutral-950">
+      <header className="border-b border-border">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-center">
+          <h1 className="text-3xl font-bold text-foreground text-white">Minecraft Server Admin Panel</h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <AdminPanel />
+        </div>
+      </main>
+    </div>
   )
 }
