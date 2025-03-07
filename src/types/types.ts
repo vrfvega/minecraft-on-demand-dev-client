2d456d53dea7fa5bd3ca51fdd07c36e77826ddeb
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-03-07 16:51:36 -0500

    update server response types

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,24 +8,51 @@ export interface ApiError {
   status: number;
 }
 
+interface MinecraftPlayer {
+  uuid: string;
+  name_clean: string;
+}
+
+export interface MinecraftServerStatus {
+  online: boolean;
+  host: string;
+  port: number;
+  eula_blocked: boolean;
+  retrieved_at: number;
+  expires_at: number;
+  version: {
+    name_raw: string;
+    name_clean: string;
+    name_html: string;
+    protocol: number;
+  };
+  players: {
+    online: number;
+    max: number;
+    list: MinecraftPlayer[];
+  };
+  motd: {
+    raw: string;
+    clean: string;
+    html: string;
+  };
+  icon: string | null;
+  mods:
+    | {
+        name: string;
+        version: string;
+      }[]
+    | null;
+}
+
+// Adjust this to match any custom player data you might want to add
 export interface Player {
-  id: string;
-  name: string;
+  uuid: string;
+  name_clean: string;
 }
 
 export interface PlayersInfo {
   online: number;
   max: number;
   list: Player[];
-}
-
-interface modInfo {
-  version: string;
-  name: string;
-}
-
-export interface ServerInfo {
-  online: boolean;
-  players: PlayersInfo;
-  mods: modInfo[];
 }
