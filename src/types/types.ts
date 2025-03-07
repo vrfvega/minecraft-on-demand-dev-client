fc8131f9401afb77564b4f64c78289bbdf4cbbed
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-03-07 13:36:28 -0500

    add types for server fetch

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,31 @@
 export interface ServerStatus {
-  taskStatus: string
-  serverIp: string
+  taskStatus: string;
+  serverIp: string;
 }
 
 export interface ApiError {
-  message: string
-  status: number
+  message: string;
+  status: number;
 }
 
+export interface Player {
+  id: string;
+  name: string;
+}
+
+export interface PlayersInfo {
+  online: number;
+  max: number;
+  list: Player[];
+}
+
+interface modInfo {
+  version: string;
+  name: string;
+}
+
+export interface ServerInfo {
+  online: boolean;
+  players: PlayersInfo;
+  mods: modInfo[];
+}
